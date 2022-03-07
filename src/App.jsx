d7a211c39c6dc9ac506b915fdb853ce6ee0e6b05
oArthur <arthursouzamaciel@gmail.com>
2022-03-06 21:27:36 -0300

    Card contador implementado

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,14 @@ import UsuarioInfo from "./components/condicional/UsuarioInfo"
 import DiretaPai from "./components/comunicacao/DiretaPai"
 import IndiretaPai from "./components/comunicacao/IndiretaPai";
 import Input from "./components/formulario/Input";
-
+import Contador from "./components/contador/Contador";
 export default _ =>
     <div className="App">
         <h1>Fundamentos React</h1>
         <div className="Cards">
+        <Card titulo="Contador" color="#99A799">
+                <Contador numeroInicial={100} />
+            </Card>
             <Card titulo="Componente Controlado" color="#B983FF">
                 <Input />
             </Card>
